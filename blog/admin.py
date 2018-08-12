ed3654855a61db6087437190f2d27e3c7a2bb554
pupubaba <kaki1203@naver.com>
2018-08-12 22:10:08 +0900

    blog.models.Post 모델이 status 필드를 추가하고, 이를 처리하는 Custom Admin Action 구현

--- blog/admin.py
+++ blog/admin.py
@@ -6,10 +6,21 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'craeted_at', 'updated_at']
+    list_display = ['id', 'title', 'status', 'created_at', 'updated_at']
+    actions = ['make_draft', 'make_published']
 
     def content_size(self, post):
         return mark_safe('<strong>{}</strong>글자'.format(len(post.content)))
-    content_size.short_description = '글자수''
+    content_size.short_description = '글자수'
 
+    def make_draft(self, request, queryset):
+        updated_count = queryset.update(status='d')
+        self.message_user(request, '{}거의 포스팅을 Draft상태로 변경'.format(updated_count))
+    make_draft.short_description = '지정 포스팅을 Draft상티로 변경합니다.'
+
+
+    def make_published(self, request, queryset):
+        updated_count = queryset.update(status='p')
+        self.message_user(request, '{}거의 포스팅을 Published상태로 변경'.format(updated_count))
+    make_published.short_description = '지정 포스팅을 Published상티로 변경합니다.'
 # admin.site.register(Post)
